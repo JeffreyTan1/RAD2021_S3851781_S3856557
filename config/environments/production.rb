be483988e91838b196b754e001021ad18367e0a5
Jeffrey <s3851781@student.rmit.edu.au>
2021-05-22 13:44:16 +0000

    putting env in the open

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,8 +95,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'example.com',
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
+    user_name:            'RailMail555@gmail.com',
+    password:             'RailMailPassword555',
     authentication:       'plain',
     enable_starttls_auto: true }
     
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.credentials.dig(:twitter, :api_key), Rails.application.credentials.dig(:twitter, :api_secret),
+  #provider :twitter, Rails.application.credentials.dig(:twitter, :api_key), Rails.application.credentials.dig(:twitter, :api_secret),
+  provider :twitter, 'IGpEfygl6nCCBI6LnollTcbSG', 'ZP7OQSvTD6SrDTvBf5q6PhilA0ucg95A6SbaiVNoX39CsgU2uX',
   {
       :authorize_params => {
         :force_login => 'true',
       }
     }
-end
+endfind 
